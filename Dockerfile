4e19008da6dd71493179d1861ec5766854e6e27a
Luis Mendes <luis.mendes1@vodafone.com>
2020-02-03 23:57:58 +0000

    Websockets implemented without DB integration; Simple Vue.js client added; Dockerize App and Mongo

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:latest
+
+RUN mkdir -p /usr/src/app
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+# wildcard for both package.json and package-lock.json
+COPY package*.json ./
+
+RUN npm install
+
+# Bundle app source
+COPY . .
+
+EXPOSE 3000
+# If running for production uncomment
+# CMD [ "npm", "run","init" ]
+
+
+CMD [ "npm", "run","start:dev" ]
--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,9 +1,11 @@
-import { SubscribeMessage, WebSocketGateway, OnGatewayInit, WsResponse, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, OnGatewayInit, WsResponse, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway()
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
 
   private logger: Logger = new Logger('AppGateway')
 
@@ -15,11 +17,11 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
     this.logger.log(`Client disconnected: ${client.id}`)
   }
   handleConnection(client: Socket, ...args: any[]) {
-    this.logger.log(`Client disconnected: ${client.id}`)
+    this.logger.log(`Client connected: ${client.id}`)
   }
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: any): WsResponse<string> {
-    return { event: 'msgToClient', data: payload }; //Whateve is in data should match the type up above on WsResponse
+  handleMessage(client: Socket, text: string): void {
+    this.wss.emit('msgToClient', text)
   }
 }
